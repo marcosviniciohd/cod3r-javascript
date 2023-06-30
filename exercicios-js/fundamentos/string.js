355f8749f33e6ee440a7dd6a67db3f747e3ee108
Marcos <marcosviniciohd@hotmail.com>
2023-06-30 01:01:57 -0300

    strings em javascript

---
+++ exercicios-js/fundamentos/string.js
@@ -0,0 +1,11 @@
+const escola = "Cod3r";
+console.log(escola.charAt(4)); // r
+console.log(escola.charAt(5)); 
+console.log('__________________________________________________');
+console.log(escola.charCodeAt(3)); // 51
+console.log('__________________________________________________');
+console.log(escola.indexOf('3')); // 3
+console.log('__________________________________________________');
+console.log(escola.substring(1)); // od3r
+console.log('__________________________________________________');
+console.log(escola.substring(0, 3)); // Cod
