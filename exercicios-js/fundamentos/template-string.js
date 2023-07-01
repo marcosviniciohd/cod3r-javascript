dbd764098dd15b5279befa216b5db3945e28326d
Marcos <marcosviniciohd@hotmail.com>
2023-06-30 22:32:53 -0300

    Template String

---
+++ exercicios-js/fundamentos/template-string.js
@@ -0,0 +1,11 @@
+const nome = 'Rebeca';
+const concatenacao = 'Olá ' + nome + '!';
+const template = `
+    Olá
+    ${nome}!`;
+console.log(concatenacao, template);
+
+// expressoes
+console.log(`1 + 1 = ${1 + 1}`);
+console.log(`10 + 10 = ${10+10}`);
+console.log(`10 * 10 = ${10*10}`);
